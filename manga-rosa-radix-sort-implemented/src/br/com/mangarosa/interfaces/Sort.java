3e3b0af98022837528f597c7c97d10fadc3702c2
Lorena-Rios <lorenadejesusrios@gmail.com>
2024-09-08 19:34:54 -0300

    Add files via upload

---
+++ manga-rosa-radix-sort-implemented/src/br/com/mangarosa/interfaces/Sort.java
@@ -0,0 +1,8 @@
+package br.com.mangarosa.interfaces;
+
+import java.util.List;
+
+public interface Sort<T extends Comparable<? super T>> {
+    List<T> sort(List<T> dataset);
+    int numberComparisons();
+}
